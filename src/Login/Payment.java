1550005daae4fc82540fc9ed8dc3a1438985ed7f
Bilal0313 <Bilalahmat222@gmail.com>
2023-11-12 15:24:27 -0500

    Payment.java has been Complicated

--- src/Login/Payment.java
+++ src/Login/Payment.java
@@ -5,7 +5,8 @@ public class Payment {
     public static void main(String[] args) {
 
         System.out.println("Payment Added");
-
+        System.out.println("Payment Updated");
+        System.out.println("Payment Complicated");
 
 
 
